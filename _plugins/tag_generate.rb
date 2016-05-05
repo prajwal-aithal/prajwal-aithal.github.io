df47dddb00b824167a79a66431c47176516748c1
Prajwal A N <prajwal.an92@gmail.com>
2016-05-05 13:25:54 +0900

    site: updated site to Jekyll 3.0
    * changed configuration to Jekyll 3.0 style
    * changed structure of tag_generate plugin
    
    Signed-off-by: Prajwal A N <prajwal.an92@gmail.com>

--- _plugins/tag_generate.rb
+++ _plugins/tag_generate.rb
@@ -1,63 +1,69 @@
 puts 'Generating tags...'
-require 'rubygems'
-require 'jekyll'
+
 include Jekyll::Filters
 
-options = Jekyll.configuration({})
-site = Jekyll::Site.new(options)
-site.read_posts('')
-s1 = Set.new []
-html =<<-HTML
----
-layout: default
-title: Tags
----
-<script type="text/javascript">
-    function toggle_visibility(id) {
-       var e = document.getElementById(id);
-       if(e.style.display == 'block')
-          e.style.display = 'none';
-       else
-          e.style.display = 'block';
-    }
-</script>
+module Jekyll
+	class TagPage < Page
+		def initialize(site, base, dir)
+			@site = site
+			@base = base
+			@dir = dir
+			@name = 'tags.html'
 
-<h1>Tags</h1>
-HTML
-site.posts.each do |post|
-  post.tags.each do |tag|
-    s1 << tag
-  end
-end
-html << <<-HTML
-HTML
-html << '<ul class="posts">'
-s1.each do |s|
-  html << <<-HTML
-  <li class="tag_list">
-    &nbsp &raquo; &nbsp <a name="#{s}" href="#" onclick="toggle_visibility('#{s}')">#{s}</a>
-    <ul id="#{s}" class="intag_list" style="display:none">
-    HTML
-    site.posts.each do |tpost|
-      tpost.tags.each do |t|
-        if t == s
-          html << <<-HTML
-            <li style="list-style-type: none;">
-              &nbsp &nbsp &nbsp &nbsp &curren; &nbsp <span style="color:#404040">#{date_to_string(tpost.date)}</span> &nbsp &raquo; &nbsp <a href="#{tpost.url}">#{tpost.title}</a>
-            </li>
-          HTML
-        end
-      end
-    end
-    html << <<-HTML
-    </ul>
-  </li>
-  HTML
-end
-html << '</ul>'
+			self.process(@name)
+			self.read_yaml(File.join(base, '_layouts'), 'tag.html')
 
-File.open('tags.html', 'w+') do |file|
-  file.puts html
+			tag_set = Set.new []
+			site.posts.docs.each do |post|
+				post.data['tags'].each do |tag|
+				tag_set << tag
+			  end
+			end
+
+			content_html = String.new
+			content_html << '<ul class="posts">'
+
+			tag_set.each do |curr_tag|
+				content_html << <<-HTMLEND
+
+	<li class="tag_list">
+		&nbsp &raquo; &nbsp <a name="#{curr_tag}" href="#" onclick="toggle_visibility('#{curr_tag}')">#{curr_tag}</a>
+		<ul id="#{curr_tag}" class="intag_list" style="display:none">
+				HTMLEND
+
+				site.posts.docs.each do |tpost|
+					tpost.data['tags'].each do |tag|
+						if curr_tag == tag
+							content_html << <<-HTMLEND
+			<li style="list-style-type: none;">
+				&nbsp &nbsp &nbsp &nbsp &curren; &nbsp <span style="color:#404040">
+					#{date_to_string(tpost.date)}
+				</span> &nbsp &raquo; &nbsp <a href="#{tpost.url}">#{tpost.data['title']}</a>
+			</li>
+							HTMLEND
+						end
+					end
+				end
+
+				content_html << <<-HTMLEND
+		</ul>
+	</li>
+				HTMLEND
+			end
+
+			content_html << "</ul>"
+
+			self.content = content_html
+		end
+	end
+
+	class TagGenerator < Generator
+		safe true
+
+		def generate(site)
+		  site.pages << TagPage.new(site, site.source, site.source)
+		end
+	end
 end
 
-puts 'Done.'
+puts 'Tag generation done.'
